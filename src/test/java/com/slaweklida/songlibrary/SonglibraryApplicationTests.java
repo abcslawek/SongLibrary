0b48e826952b66de85e0d0a3ab79bdfbbf197bad
Slawek Lida <slaweklida@tlen.pl>
2023-07-11 14:21:57 +0200

    pierwszy commit

---
+++ src/test/java/com/slaweklida/songlibrary/SonglibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slaweklida.songlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonglibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
